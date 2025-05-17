448703f2ebe907caa40a76b5f7bda09e71b3bc88
bajputk <utkarsh@decoverhq.com>
2025-05-17 23:41:29 +0530

    working now

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@cn/button'
+
+export const App = () => {
+  return <Button variant='outline'>shadcn</Button>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root") as HTMLElement);
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
+const root = createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
